81d341487b5e5ded38fd1ac24144ac00aa169046
Min Dal-Pong <45223821+alsgpwns@users.noreply.github.com>
2020-05-07 02:41:24 +0900

    43.6 Create ordinal abbreviation
    서수 줄임말 만들기

---
+++ Ordinal_number.c
@@ -0,0 +1,28 @@
+﻿#include <stdio.h>
+
+int main() {
+
+	int number;
+	char name[31], result[40];
+
+	printf("문자열을 입력해주세요.\n");
+	scanf("%d %s", &number, name);
+
+	if (number == 1) {
+		sprintf(result, "%dst %s", number, name);
+	}
+
+	else if (number == 2) {
+		sprintf(result, "%dnd %s", number, name);
+	}
+	else if (number == 3) {
+		sprintf(result, "%drd %s", number, name);
+	}
+
+	else {
+		sprintf(result, "%dth %s", number, name);
+	}
+
+
+	printf("%s \n", result);
+}
