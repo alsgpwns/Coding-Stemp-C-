820a55cedd8c6587a224dd0ef544363c2952203a
Min Dal-Pong <45223821+alsgpwns@users.noreply.github.com>
2020-05-07 02:38:04 +0900

    45-8. Counting Specific Words
    [심사문제]45.8: 특정 단어 갯수 세기

---
+++ Counting_Specific_Words.c
@@ -0,0 +1,31 @@
+﻿#include <stdio.h>
+#include <string.h>
+
+void main() {
+
+	char s1[1001]= "";
+	char s2[] = "the";
+	char* test = NULL;
+
+	int count=0;
+
+	printf("문자열을 입력해주세요.\n");
+	scanf_s("%[^\n]", s1,1001);
+
+	char* ptr = strtok_s(s1, "'., ",&test);
+
+	while (ptr != NULL) {
+
+		
+		if (!strcmp(ptr,s2)) {
+			count = count + 1;
+		}
+		ptr = strtok_s(NULL, "'., ",&test);
+		
+
+	}
+
+
+
+	printf("%d", count);
+}
