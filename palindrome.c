6eb95129918462b2b42c941abc7d72521559ff22
Min Dal-Pong <45223821+alsgpwns@users.noreply.github.com>
2020-05-07 02:56:58 +0900

    47.5 Determination of palindrome
    47.5 회문 판별

---
+++ palindrome.c
@@ -0,0 +1,26 @@
+﻿#include <stdio.h>
+
+#include <string.h>
+#include <stdbool.h>
+
+void main() {
+
+	char s1[31];
+	int length;
+	bool re = true;
+
+	printf("문자열을 입력하세요. \n");
+	scanf_s("%s", s1,31);
+
+	length = strlen(s1);
+
+	for (int i = 0; i < length / 2; i++) {
+
+		if (s1[i] != s1[length - i - 1]) {
+			re = false;
+			break;
+		}
+	}
+	printf("%d", re);
+
+}
